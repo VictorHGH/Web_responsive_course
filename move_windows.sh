002f01203dc2e2f5a151b0084bd24ca478feaad5
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-04-24 22:43:24 -0600

    New config for 2 monitor in MacOS

--- move_windows.sh
+++ move_windows.sh
@@ -10,8 +10,10 @@ move_to_second_monitor_linux() {
 
   # Get the window IDs of the Chrome windows and Terminal
   chrome_wmctrl_ids=($(wmctrl -l | grep "Google Chrome" | awk '{print $1}'))
-  chrome_xdotools_ids=($(xdotool search --class --onlyvisible "google-chrome"))
   terminal_wmctrl_id=$(wmctrl -l | grep "Terminal" | awk '{print $1}')
+  
+  # Get the xdotools IDs of the Chrome windows and Terminal
+  chrome_xdotools_ids=($(xdotool search --class --onlyvisible "google-chrome"))
   terminal_xdotools_id=$(xdotool search --onlyvisible --class "Terminal")
 
   # Change window sizes with xdotool
@@ -32,8 +34,8 @@ move_to_second_monitor_linux() {
 
 # Function to move window to second monitor for macOS
 move_to_second_monitor_mac() {
-  open -a "Google Chrome" --args --new-window "https://www.udemy.com/"
-  open -a "Safari" --args --new-window "http://localhost:5173"
+  open -a "Google Chrome" "https://www.udemy.com/"
+  open -a "Safari" "http://localhost:5173"
   open -a "Terminal"
 
   # Get the window ID of the Chrome window
@@ -42,13 +44,13 @@ move_to_second_monitor_mac() {
   terminal_window_id=$(osascript -e 'tell application "Terminal" to id of window 1')
 
   # Set the new window size (width x height)
-  chrome_new_width=5760
+  chrome_new_width=3840
   chrome_new_height=1080
 
-  safari_new_width=3840
+  safari_new_width=1920
   safari_new_height=1080
 
-  terminal_new_width=1920
+  terminal_new_width=960
   terminal_new_height=1080
 
   # Move the window to the second monitor
@@ -56,8 +58,8 @@ move_to_second_monitor_mac() {
   # the left and top coordinates are set to 1920 and 0, respectively, indicating the position of the top-left corner 
   # of the window on the screen. The right and bottom coordinates are calculated based on the width and height of the 
   # window ($chrome_new_width and $chrome_new_height variables).
-  osascript -e "tell application \"Google Chrome\" to set bounds of window id $chrome_window_id to {3840, 0, $chrome_new_width, $chrome_new_height}"
-  osascript -e "tell application \"Safari\" to set bounds of window id $safari_window_id to {1920, 0, $safari_new_width, $safari_new_height}"
+  osascript -e "tell application \"Google Chrome\" to set bounds of window id $chrome_window_id to {1920, 0, $chrome_new_width, $chrome_new_height}"
+  osascript -e "tell application \"Safari\" to set bounds of window id $safari_window_id to {960, 0, $safari_new_width, $safari_new_height}"
   osascript -e "tell application \"Terminal\" to set bounds of window id $terminal_window_id to {0, 0, $terminal_new_width, $terminal_new_height}"
 }
 
